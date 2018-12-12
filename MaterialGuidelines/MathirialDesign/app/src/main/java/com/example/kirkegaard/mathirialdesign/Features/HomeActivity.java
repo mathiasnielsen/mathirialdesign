9f9bf744c106d434394398eb6cdc5fd508d3fa69
Mathias Kirkegaard Nielsen <matn@bluefragments.com>
2018-12-12 15:09:12 +0100

    Player ground

--- MaterialGuidelines/MathirialDesign/app/src/main/java/com/example/kirkegaard/mathirialdesign/Features/HomeActivity.java
+++ MaterialGuidelines/MathirialDesign/app/src/main/java/com/example/kirkegaard/mathirialdesign/Features/HomeActivity.java
@@ -48,7 +48,8 @@ public class HomeActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                NavigateToPage(SensorActivity.class);
+                //NavigateToPage(SensorActivity.class);
+                NavigateToPage(PlaygroundActivity.class);
             }
         });
 
